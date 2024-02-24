0317e0844878005af5a29db73ec0c78c3f6f8c65
OldTyT <nezlobin118@gmail.com>
2024-02-24 19:49:34 +0300

    test: to ssh id_ed25519

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "$SSH_KEY_PRIVATE" > ~/.ssh/id_rsa && \
+echo "$SSH_KEY_PRIVATE" > ~/.ssh/id_ed25519 && \
 chmod 600 /root/.ssh/id_rsa && \
-echo "$SSH_KEY_PUBLIC" > ~/.ssh/id_rsa.pub && \
+echo "$SSH_KEY_PUBLIC" > ~/.ssh/id_ed25519.pub && \
 python3 /app/main.py
